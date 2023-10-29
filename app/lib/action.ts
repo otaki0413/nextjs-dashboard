a935e1d1a741868dd376012cc2dfe06cc49fd05f
otaki0413 <takida0413@gmail.com>
2023-10-29 14:16:13 +0900

    ✨ 14_アクセシビリティ向上

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -5,11 +5,18 @@ import { sql } from "@vercel/postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
+// 請求書のスキーマ
 const InvoiceSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(["pending", "paid"]),
+  customerId: z.string({
+    invalid_type_error: "Please select a customer.",
+  }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: "Please enter an amount greater than $0." }),
+  status: z.enum(["pending", "paid"], {
+    invalid_type_error: "Please select an invoice status.",
+  }),
   date: z.string(),
 });
 
@@ -17,13 +24,34 @@ const CreateInvoice = InvoiceSchema.omit({ id: true, date: true });
 const UpdateInvoice = InvoiceSchema.omit({ date: true });
 const DeleteInvoice = InvoiceSchema.pick({ id: true });
 
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+
 // 請求書の新規作成
-export async function createInvoice(formData: FormData) {
-  const { customerId, amount, status } = CreateInvoice.parse({
+export async function createInvoice(prevState: State, formData: FormData) {
+  // フォームフィールドのバリデーション
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
     status: formData.get("status"),
   });
+
+  // バリデーション失敗時、エラーを返す
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields. Failed to Create Invoice.",
+    };
+  }
+
+  // インサート用のデータ用意
+  const { customerId, amount, status } = validatedFields.data;
   const amountToCents = amount * 100;
   const date = new Date().toISOString().split("T")[0];
 
@@ -38,6 +66,7 @@ export async function createInvoice(formData: FormData) {
     };
   }
 
+  // ページの再検証とリダイレクト
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 }
@@ -68,8 +97,6 @@ export async function updateInvoice(formData: FormData) {
 
 // 請求書の削除
 export async function deleteInvoice(formData: FormData) {
-  throw new Error("Failed to Delete Invoice");
-
   const { id } = DeleteInvoice.parse({ id: formData.get("id") });
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
